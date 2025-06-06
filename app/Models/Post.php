1b730c50411af503a2a7ca13f38ddd02ce43629c
Rafael Batista <rafael@devbatista.com>
2025-06-06 16:59:21 -0300

    ajustando relacionamento entre tabelas

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasUuid;
+
+    public function author(): BelongsTo 
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
