2c3707cb4bd595bb99b8ca2d76ce5074ed3e2243
Rafael Batista <rafael@devbatista.com>
2025-06-08 17:02:05 -0300

    ajuste de migration com uuid

--- database/migrations/2025_06_08_190401_create_post_tag_table.php
+++ database/migrations/2025_06_08_190401_create_post_tag_table.php
@@ -12,9 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
-            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
+            $table->uuid('post_id');
+            $table->uuid('tag_id');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
         });
     }
 
