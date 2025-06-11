db0a512735a4334b3e30bfac3026034a0a405421
Rafael Batista <rafael@devbatista.com>
2025-06-11 16:58:18 -0300

    criando rota pra buscar os posts de um admin

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -22,3 +23,7 @@ Route::prefix('auth')->group(function() {
 Route::get('/posts', [PostController::class, 'getPosts']);
 Route::get('posts/{slug}', [PostController::class, 'getPost']);
 Route::get('posts/{slug}/related', [PostController::class, 'getRelatedPosts']);
+
+Route::prefix('admin')->middleware('auth:sanctum')->group(function() {
+    Route::get('/posts', [AdminController::class, 'getPosts']);
+});
