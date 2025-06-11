e6e96d6139df1146c390ef3384d81e0fee46b1c3
Rafael Batista <rafael@devbatista.com>
2025-06-11 17:05:45 -0300

    acertando o retorno de posts

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,6 +9,21 @@ class AdminController extends Controller
     public function getPosts(Request $request)
     {
         $user = $request->user();
-        return $user;
+
+        $posts = $user->posts->map(function($post){
+            return [
+                'id' => $post->id,
+                'title' => $post->title,
+                'created_at' => $post->created_at,
+                'cover' => $post->cover,
+                'author_name' => $post->author->name,
+                'tags' => $post->tags->implode('name', ', '),
+                'content' => $post->content,
+                'slug' => $post->slug,
+                'status' => $post->status
+            ];
+        });
+
+        return $posts;
     }
 }
